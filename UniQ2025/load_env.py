f10e33ae7de7b3fbcc04b7432601e2122ca56a61
UniQuantum2025 <lizsalander2022@gmail.com>
2025-04-25 22:54:15 -0400

    Removed embedded repository and added files

--- UniQ2025/load_env.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from the .env file
-load_dotenv()
-
-# Access the variables
-secret_key = os.getenv('SECRET_KEY')
-api_url = os.getenv('API_URL')
